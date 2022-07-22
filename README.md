db5a1770fabc924363f9ba1f2a080158b6e53b3f
Isaac <icorbrey@gmail.com>
2022-07-22 01:49:12 -0400

    feat: Add video interface

---
+++ README.md
@@ -0,0 +1,7 @@
+# trace64
+
+A raytracer demo built for the Nintendo 64.
+
+## Credits
+
+* https://github.com/PeterLemon/N64
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![no_main]
+#![no_std]
+
+mod hardware;
+
+use core::panic::PanicInfo;
+use hardware::video_interface;
+
+#[no_mangle]
 fn main() {
-    println!("Hello, world!");
+    video_interface::init();
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    loop {}
 }
